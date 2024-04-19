2fe44e441ac80fd394f85600d54c3fbf309c9d47
LinhTQ <linhtq@vtconline.vn>
2024-04-19 17:32:52 +0700

    news page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,40 @@
-$(function () {
+$(function () {});
 
-});
+function showSubMenu(id) {
+  var idSubmenuSelected = "#" + id + "-sub-menu";
+
+  if ($(idSubmenuSelected).css("display") == "none") {
+    $(idSubmenuSelected).show();
+  } else {
+    $(idSubmenuSelected).hide();
+  }
+}
+
+function showMenuMobile() {
+  var menuMobileElm = $(".menu-mobile");
+
+  if ($(menuMobileElm).css("display") == "none") {
+    $(menuMobileElm).show();
+  } else {
+    $(menuMobileElm).hide();
+  }
+}
+
+function tabOpen(idHeaderSelected, idContentSelected) {
+  var tabContent, tabHeader;
+  var idHeaderSelectedQuerry = "#" + idHeaderSelected;
+  var idContentSelectedQuerry = "#" + idContentSelected;
+
+  tabContent = $(".tabs__contents--items");
+  tabContent.each((index, value) => {
+    $(value).hide();
+  });
+
+  tabHeader = $(".tabs__header--items");
+  tabHeader.each((index, value) => {
+    $(value).removeClass("tabs__header--active");
+  });
+
+  $(idHeaderSelectedQuerry).addClass("tabs__header--active");
+  $(idContentSelectedQuerry).show().addClass("tabs__contents--active");
+}
