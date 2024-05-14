49fc349176c029cc96aab0a61585fbf762506675
LinhTQ <linhtq@vtconline.vn>
2024-05-14 11:37:32 +0700

    audio pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,12 +19,13 @@ $(function () {
     autoplaySpeed: 5000,
     prevArrow: false,
     nextArrow: false,
-  }); 
+  });
+
+  initControlsAudio("story-3");
 });
 
 function showSubMenu(id) {
   var idSubmenuSelected = "#" + id + "-sub-menu";
-
   if ($(idSubmenuSelected).css("display") == "none") {
     $(idSubmenuSelected).show();
   } else {
@@ -64,4 +65,44 @@ function tabOpen(idSection, idHeaderSelected, idContentSelected) {
 
   $(idHeaderSelectedQuerry).addClass("tabs__header--active");
   $(idContentSelectedQuerry).show().addClass("tabs__contents--active");
+}
+
+function initControlsAudio(idStory) {
+  var idStorySelector = "#" + idStory;
+  var idBtnPlay = idStorySelector + " #btn-play";
+  var idProgressTime = idStorySelector + " .progress-time";
+  var audioElm = idStorySelector + " audio";
+  var idBtnPause = idStorySelector + " #btn-pause";
+  var idTimeCurrent = idStorySelector + " #time--current";
+  var setIntervalTime;
+  $(idBtnPlay).click(() => {
+    $(idBtnPlay).hide();
+    $(idBtnPause).show();
+    $(idProgressTime).css({
+      display: "flex",
+    });
+    $(audioElm)[0].play();
+    setIntervalTime = setInterval(() => {
+      $(idTimeCurrent).html(Math.round($(audioElm)[0].currentTime));
+      console.log(Math.round($(audioElm)[0].currentTime));
+    }, 1000);
+  });
+
+  $(idBtnPause).click(() => {
+    $(idBtnPlay).show();
+    $(idBtnPause).hide();
+    clearInterval(setIntervalTime);
+    $(audioElm)[0].pause();
+  });
+}
+
+function formatTimeAudio(time) {
+  var minute = time/60;
+  
+
+  var minute = 0;
+
+if(time < 10) {
+
+}
 }
